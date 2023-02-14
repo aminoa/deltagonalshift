6be6ca315ef48c887d4c0056be6a156a2122ec61
aminoa <28660350+aminoa@users.noreply.github.com>
2023-02-14 13:17:45 -0500

    deltashift works now

--- trace_tests.py
+++ trace_tests.py
@@ -6,12 +6,14 @@ def trace(A):
 # A - matrix, l - number of iterations
 def hutchinson(A, l):
     trace_approx = 0
-
     for _ in range(l):
         g_vec = np.random.normal(0, 1, size=(n, ))
         trace_approx += np.dot(np.transpose(g_vec), np.dot(A, g_vec))
 
     return trace_approx / l
+
+def generate_random_rademacher(n):
+    return np.random.choice([-1, 1], size=(n, ))
 
 # n - size
 def generate_matrix(n):
@@ -36,15 +38,26 @@ def running_hutchinson_estimate(A, j, l):
         start += hutchinson(change, l)
     return start
 
-def deltashift(A, l0, m, l):
-    t1 = hutchinson(A, l0)
+def deltashift(A, l0, m, l, gamma):
+    t_prev = hutchinson(A, l0)
+    prev_matrix = A
     for _ in range(1, m):
         # create l random +1/-1 vectors 
-        g = np.array([np.random.normal(0, 1, size=(n, )) for _ in range(l)])
-        z = np.array([np.dot(np.transpose(g[i]), np.dot(A, g[i])) for i in range(l)])
+        curr_matrix = pertube_matrix(A)
+        g = np.array([generate_random_rademacher(n) for _ in range(l)]) 
+        z = np.array([np.dot(prev_matrix, g[i]) for i in range(l)])
+        w = np.array([np.dot(curr_matrix, g[i]) for i in range(l)])
 
+        right_sum = 0
+        for i in range(l):
+            right_sum += np.dot(g[i], w[i] - (1 - gamma) * z[i])
+        right_sum /= l
 
+        t_curr = (1 - gamma) * t_prev + right_sum
+        prev_matrix = curr_matrix
+        t_prev = t_curr
 
+    return t_curr
 
 n = 10
 l = 1000
@@ -55,3 +68,4 @@ print(np.trace(test))
 print(hutchinson(test, l))
 print(running_trace_estimate(test, run_iter))
 print(running_hutchinson_estimate(test, l, run_iter))
+print(deltashift(test, l, run_iter, l, 0.1))
