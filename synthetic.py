80b43b69ebcd5782969ac2de020ac6005a4bd1e3
aminoa <28660350+aminoa@users.noreply.github.com>
2023-04-25 13:10:09 -0400

    synthetic tests

--- synthetic.py
+++ synthetic.py
@@ -8,7 +8,7 @@ import time
 
 # repeated hutchinson's diagonal estimator vs. deltagonalshift with small matrix pertubation
 
-def repeated_diagonal_estimate_test(iterations, small_pertube):
+def repeated_diagonal_estimate_separate_test(iterations, small_pertube):
     A = generate_matrix(100) 
     start = time.time()
     result, true = repeated_diagonal_estimator(A, 100, iterations, small_pertube)
@@ -18,7 +18,7 @@ def repeated_diagonal_estimate_test(iterations, small_pertube):
 
     return result, true, total_time
 
-def deltagonalshift_estimate_test(iterations, small_pertube, gamma):
+def deltagonalshift_estimate_separate_test(iterations, small_pertube, gamma):
     A = generate_matrix(100)
     start = time.time()
     result, true = deltashift(A, 100, iterations, 10, gamma, small_pertube)
@@ -27,6 +27,22 @@ def deltagonalshift_estimate_test(iterations, small_pertube, gamma):
     total_time = end - start
 
     return result, true, total_time
+
+
+def repeated_diagonal_deltagonalshift_test(iterations, small_pertube, gamma):
+    A = generate_matrix(100) 
+    repeated_start = time.time()
+    repeated_result, repeated_true = repeated_diagonal_estimator(np.matrix.copy(A), 100, iterations, small_pertube)
+    repeated_end = time.time()
+    repeated_total_time = repeated_end - repeated_start
+                
+    deltagonal_start = time.time()
+    deltagonal_result, deltagonal_true = deltagonalshift(np.matrix.copy(A), 100, iterations, 10, gamma, small_pertube)
+    deltagonal_end = time.time()
+    deltagonal_total_time = deltagonal_end - deltagonal_start
+
+    return repeated_result, repeated_true, repeated_total_time, deltagonal_result, deltagonal_true, deltagonal_total_time
+
 
 def calculate_error(result, true):
     error = [0 for _ in range(len(result))]
@@ -41,14 +57,24 @@ def plot_error(error):
     plt.ylabel('Relative error')
     plt.show()
 
-repeated_hutchinson_output = repeated_diagonal_estimate_test(100, True)
-print(repeated_hutchinson_output[2])
-repeated_hutchinson_error = calculate_error(repeated_hutchinson_output[0], repeated_hutchinson_output[1])
-plot_error(repeated_hutchinson_error)
+repeated_result, repeated_true, repeated_total_time, deltagonal_result, deltagonal_true, deltagonal_total_time = repeated_diagonal_deltagonalshift_test(1000, True, 0.1)
 
-deltagonalshift_output = deltagonalshift_estimate_test(100, True, 0.1)
-print(deltagonalshift_output[2])
-deltagonalshift_error = calculate_error(deltagonalshift_output[0], deltagonalshift_output[1])
-plot_error(deltagonalshift_error)
+print(repeated_total_time)
+repeated_error = calculate_error(repeated_result, repeated_true)
+plot_error(repeated_error)
+
+print(deltagonal_total_time)
+deltagonal_error = calculate_error(deltagonal_result, deltagonal_true)
+plot_error(deltagonal_error)
+
+# repeated_hutchinson_output = repeated_diagonal_estimate_test(100, True)
+# print(repeated_hutchinson_output[2])
+# repeated_hutchinson_error = calculate_error(repeated_hutchinson_output[0], repeated_hutchinson_output[1])
+# plot_error(repeated_hutchinson_error)
+
+# deltagonalshift_output = deltagonalshift_estimate_test(100, True, 0.1)
+# print(deltagonalshift_output[2])
+# deltagonalshift_error = calculate_error(deltagonalshift_output[0], deltagonalshift_output[1])
+# plot_error(deltagonalshift_error)
 
 # plt.show()
